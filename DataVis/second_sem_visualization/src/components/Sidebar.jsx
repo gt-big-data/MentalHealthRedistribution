6f914f4a9bb832510c3baf6b69f3e48f6c34550b
Edmund Xin <mxinburritos@gmail.com>
2021-04-15 02:21:41 -0700

    add mental health center waypoints

--- DataVis/second_sem_visualization/src/components/Sidebar.jsx
+++ DataVis/second_sem_visualization/src/components/Sidebar.jsx
@@ -9,9 +9,9 @@ const Sidebar = () => {
   const { selectedCounty, setSelectedCounty } = useContext(CountyContext);
 
   return (
-    <Fade in={selectedCounty.length >= 1}>
-      <Flex direction='column' position='fixed' h='80vh' w='20vw' top='10vh' left='5vw' backgroundColor='#fff' borderRadius='20px' py={5} px={5} boxShadow='0px 4px 10px rgba(106, 166, 255, 0.4)'>
-        { selectedCounty.length < 1 ? <></> :
+    <Fade in={selectedCounty.length > 0}>
+      { selectedCounty.length < 1 ? <></> :
+        <Flex direction='column' position='fixed' h='80vh' w='20vw' top='10vh' left='5vw' backgroundColor='#fff' borderRadius='20px' py={5} px={5} boxShadow='0px 4px 10px rgba(106, 166, 255, 0.4)'>
           <>
             <Heading>{`${selectedCounty[0].properties.name} County`}</Heading>
             <Text>{selectedCounty[0].index}</Text>
@@ -19,8 +19,8 @@ const Sidebar = () => {
               <CloseIcon/>
             </Button>
           </>
-        }
-      </Flex>
+        </Flex>
+      }
     </Fade>
   );
 };
