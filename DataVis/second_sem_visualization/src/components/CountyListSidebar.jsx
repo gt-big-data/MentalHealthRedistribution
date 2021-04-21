aa153120debb0e1cd53dacddef6a9fe81ab794bc
Edmund Xin <mxinburritos@gmail.com>
2021-04-20 23:49:27 -0700

    add api call

--- DataVis/second_sem_visualization/src/components/CountyListSidebar.jsx
+++ DataVis/second_sem_visualization/src/components/CountyListSidebar.jsx
@@ -1,14 +1,23 @@
 import React, { useContext } from 'react';
 import { Flex, Button, Fade, Box } from '@chakra-ui/react';
+import axios from 'axios';
 
 import { CountyContext } from '../contexts';
 import CountyListEntry from './CountyListEntry';
+
+const url = 'http://mental-health-redistribution.uc.r.appspot.com';
 
 const Sidebar = () => {
   // eslint-disable-next-line no-unused-vars
   const { selectedCounties, setSelectedCounties } = useContext(CountyContext);
 
-  console.log(selectedCounties);
+  const getOptimalCenters = async () => {
+    const counties = ['Autauga County, Alabama', 'Baldwin County, Alabama'];
+    console.log(selectedCounties);
+    const response = await axios.get(`${url}/optimal_centers?${counties.map((n) => `counties=${n.split(' ').join('%20')}`).join('&')}`);
+
+    console.log(response);
+  };
 
   return (
     <Fade in={selectedCounties.length > 0}>
@@ -17,12 +26,11 @@ const Sidebar = () => {
           <Box boxShadow='inset 0px 4px 10px rgba(106, 166, 255, 0.4)' height='40%' borderRadius='20px' overflowY='scroll'>
             {
               selectedCounties.map((county, i) => {
-                console.log(county);
                 return <CountyListEntry key={i} county={county} />;
               })
             }
           </Box>
-          <Button position='absolute' bottom='70px' w='80%' left='50%' transform='translateX(-50%)' backgroundColor='#6AA6FF' color='#fff' onClick={() => {}}>
+          <Button position='absolute' bottom='70px' w='80%' left='50%' transform='translateX(-50%)' backgroundColor='#6AA6FF' color='#fff' onClick={() => {getOptimalCenters();}}>
                 Calculate Optimal Locations
           </Button>
           <Button position='absolute' bottom='20px' w='80%' left='50%' transform='translateX(-50%)' backgroundColor='#ff7d86' color='#fff' onClick={() => {setSelectedCounties([]);}}>
